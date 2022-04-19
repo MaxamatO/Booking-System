0031c17d7b6c2ad937de8449e9ad5e7cc255d06d
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-04-19 22:38:42 +0200

    Added new fields, working on new column @MTM

--- src/main/java/com/maxamato/bookingsystem/entities/Client.java
+++ src/main/java/com/maxamato/bookingsystem/entities/Client.java
@@ -1,32 +1,55 @@
 package com.maxamato.bookingsystem.entities;
 
 import com.maxamato.bookingsystem.dtos.HotelDto;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Setter
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private final Long id;
+    private Long clientId;
 
-    private final String email;
-    private final String password;
+    private String email;
+    private String password;
 
     @Column(name = "date_of_birth")
-    private final LocalDateTime dateOfBirth;
+    private LocalDate dateOfBirth;
 
-    private final boolean isAdult;
+    private LocalDateTime accountCreatedAt=LocalDateTime.now();
 
+    private final boolean isAdult= Period.between(dateOfBirth, LocalDate.now())
+            .getYears()>=18;
+
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinTable(
+            name = "clients_rooms",
+            joinColumns = @JoinColumn(name = "client_id"),
+            inverseJoinColumns = @JoinColumn(name = "hotel_room_id")
+    )
+    private final Set<HotelRoom> bookedRooms = new HashSet<>();
+
+    public Client(String email, String password, LocalDate dateOfBirth) {
+        this.email = email;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    private Boolean isAdult(int years){
+        return years >= 18;
+    }
 
 //    private final List<HotelDto> bookedHotels;
 
