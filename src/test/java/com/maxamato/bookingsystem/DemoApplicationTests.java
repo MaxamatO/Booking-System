9ae8f8ccfcffac40703905aab9e831337e8b12de
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-04-19 22:40:07 +0200

    removed unnecessary things - whole file lmao

--- src/test/java/com/maxamato/bookingsystem/DemoApplicationTests.java
+++ src/test/java/com/maxamato/bookingsystem/DemoApplicationTests.java
@@ -1,29 +1,17 @@
 package com.maxamato.bookingsystem;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class DemoApplicationTests {
-	Calculator underTest = new Calculator();
 
 	@Test
-	void itShouldAddNumbers() {
-		// Given
-		int numberOne = 20;
-		int numberTwo = 30;
+	void contextLoads(){
 
-		// when
-		int result = underTest.add(numberOne, numberTwo);
-
-		// then
-		final int expected = 50;
-		assertThat(result).isEqualTo(expected);
 	}
 
-	class Calculator{
-		int add(int a, int b){return a+b;}
-	}
+
 
 }
