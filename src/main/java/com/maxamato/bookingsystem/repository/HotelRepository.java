2713f51d9848866d96f287b2029331a4dc3d7200
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-04-19 22:43:38 +0200

    executeUpdate throwing an error

--- src/main/java/com/maxamato/bookingsystem/repository/HotelRepository.java
+++ src/main/java/com/maxamato/bookingsystem/repository/HotelRepository.java
@@ -1,8 +1,8 @@
 package com.maxamato.bookingsystem.repository;
 
 import com.maxamato.bookingsystem.entities.Hotel;
-import com.maxamato.bookingsystem.repository.customs.CustomizedHotelRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +10,11 @@ import java.util.List;
 
 @Transactional
 @Repository
-public interface HotelRepository extends JpaRepository<Hotel, Long>, CustomizedHotelRepository {
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
 
     public List<Hotel> findAllByCountry(String country);
+
+    @Query(nativeQuery = true, value = "update hotels h SET number_of_rooms = (SELECT COUNT(r.hotel_id) FROM hotel_room r WHERE r.hotel_id = h.hotel_id);")
     public void executeUpdate();
 
 }
