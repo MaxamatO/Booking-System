efc071f618f3ecaf8747994a692d78e26d54c5b5
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-04-25 21:35:12 +0200

    Implemented findAllClients, addClientToHotelRoom

--- src/main/java/com/maxamato/bookingsystem/controllers/ClientController.java
+++ src/main/java/com/maxamato/bookingsystem/controllers/ClientController.java
@@ -1,13 +1,13 @@
 package com.maxamato.bookingsystem.controllers;
 
 import com.maxamato.bookingsystem.entities.Client;
+import com.maxamato.bookingsystem.entities.HotelRoom;
 import com.maxamato.bookingsystem.entities.requests.ClientRequest;
 import com.maxamato.bookingsystem.services.ClientService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -15,8 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClientController {
     private final ClientService clientService;
 
-    @PostMapping
+    @GetMapping(path = "all")
+    public List<Client> findAllClients(){
+        return clientService.findAllClients();
+    }
+
+    @PostMapping(path = "add")
     public Client addClient(@RequestBody ClientRequest clientRequest) {
         return clientService.addClient(clientRequest);
     }
+
+    // NOT WORKING
+    @PutMapping(path = "{clientId}/rooms/{roomId}/add")
+    public List<HotelRoom> addClientToHotelRoom(@PathVariable Long clientId, @PathVariable Long roomId){
+        return clientService.addClientToHotelRoom(clientId, roomId);
+    }
+
+
+
 }
