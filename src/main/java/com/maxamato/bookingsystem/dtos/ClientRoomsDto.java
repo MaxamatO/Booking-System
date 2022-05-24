2c443559cdc664181733791147cacc56ccce1e15
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-05-24 22:55:58 +0200

    Deprecated

--- src/main/java/com/maxamato/bookingsystem/dtos/ClientRoomsDto.java
+++ src/main/java/com/maxamato/bookingsystem/dtos/ClientRoomsDto.java
@@ -1,9 +1,22 @@
-package com.maxamato.bookingsystem.dtos;
-
-
-import com.maxamato.bookingsystem.entities.HotelRoom;
-
-import java.util.List;
-
-public record ClientRoomsDto(String email, boolean isAdult, List<HotelRoom> hotelRooms) {
-}
+//package com.maxamato.bookingsystem.dtos;
+//
+//
+//import com.maxamato.bookingsystem.entities.HotelRoom;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import java.util.List;
+//
+//@Getter
+//@Setter
+//public class ClientRoomsDto {
+//    private String email;
+//    private boolean isAdult;
+//    private List<HotelRoomDto> hotelRooms;
+//
+//    public ClientRoomsDto(String email, boolean isAdult, List<HotelRoomDto> hotelRooms) {
+//        this.email = email;
+//        this.isAdult = isAdult;
+//        this.hotelRooms = hotelRooms;
+//    }
+//}
