980d23ad577158d7d696958f2e8a03bcb7eacf9e
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-05-28 22:53:13 +0200

    New entity for managing bookings

--- src/main/java/com/maxamato/bookingsystem/entities/Booking.java
+++ src/main/java/com/maxamato/bookingsystem/entities/Booking.java
@@ -1,13 +1,15 @@
 package com.maxamato.bookingsystem.entities;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
 public class Booking {
 
     // TODO: Client 1:M Booking M:1 HotelRoom, restructure all the code
@@ -16,6 +18,15 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    private Client client;
+
+    @ManyToOne
+    private HotelRoom hotelRoom;
 
 
+    public Booking(Client client, HotelRoom hotelRoom) {
+        this.client = client;
+        this.hotelRoom = hotelRoom;
+    }
 }
