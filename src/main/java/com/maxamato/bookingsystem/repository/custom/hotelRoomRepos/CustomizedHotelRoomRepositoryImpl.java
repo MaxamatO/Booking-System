968e6b1ae361ef980e2457c98e280346a3e066ae
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-05-28 22:50:51 +0200

    Changed executeNumberOfClientsUpdate so it works

--- src/main/java/com/maxamato/bookingsystem/repository/custom/hotelRoomRepos/CustomizedHotelRoomRepositoryImpl.java
+++ src/main/java/com/maxamato/bookingsystem/repository/custom/hotelRoomRepos/CustomizedHotelRoomRepositoryImpl.java
@@ -12,7 +12,7 @@ public class CustomizedHotelRoomRepositoryImpl implements CustomizedHotelRoomRep
 
     @Override
     public void executeNumberOfClientsUpdate() {
-        String sql = "update hotel_room hr SET number_of_clients = (SELECT COUNT(c.hotel_room_id) FROM hotel_room_client c WHERE c.hotel_room_id = hr.id);";
+        String sql = "update hotel_room hr set number_of_clients = (select count(b.hotel_room_id) from booking b where b.hotel_room_id = hr.id);";
         final Query hotelTypedQuery = entityManager.createNativeQuery(sql);
         hotelTypedQuery.executeUpdate();
     }
