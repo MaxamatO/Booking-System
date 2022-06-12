6230b7c29daf09d36d739b466c0b4d2088daccae
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-06-12 23:31:20 +0200

    Test for canAddClientToHotelRoom not working properly

--- src/test/java/com/maxamato/bookingsystem/services/ClientServiceTest.java
+++ src/test/java/com/maxamato/bookingsystem/services/ClientServiceTest.java
@@ -1,49 +1,96 @@
 package com.maxamato.bookingsystem.services;
 
+import com.maxamato.bookingsystem.dtos.BookingDto;
 import com.maxamato.bookingsystem.dtos.ClientDto;
+import com.maxamato.bookingsystem.entities.Booking;
 import com.maxamato.bookingsystem.entities.Client;
+import com.maxamato.bookingsystem.entities.Hotel;
 import com.maxamato.bookingsystem.entities.HotelRoom;
+import com.maxamato.bookingsystem.entities.requests.BookingRequest;
 import com.maxamato.bookingsystem.entities.requests.ClientRequest;
 import com.maxamato.bookingsystem.repository.BookingRepository;
 import com.maxamato.bookingsystem.repository.ClientRepository;
 import com.maxamato.bookingsystem.repository.HotelRepository;
 import com.maxamato.bookingsystem.repository.HotelRoomRepository;
 import org.checkerframework.checker.units.qual.C;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.Entity;
+import javax.transaction.Transactional;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
 class ClientServiceTest {
 
-    @Mock
+    @MockBean
     private ClientRepository clientRepository;
-    @Mock
+    @MockBean
     private HotelRoomRepository hotelRoomRepository;
-    @Mock
+    @MockBean
     private HotelRepository hotelRepository;
-    @Mock
+    @MockBean
     private BookingRepository bookingRepository;
+    @InjectMocks
     private ClientService underTest;
+
+
+    private Hotel hotel;
+    private HotelRoom hotelRoom;
+    private Client client;
+    private Booking booking;
 
     @BeforeEach
     void setUp() {
         underTest = new ClientService(clientRepository, hotelRoomRepository, hotelRepository, bookingRepository);
+
+        hotel = new Hotel();
+        hotel.setHotelName("Marriott");
+        hotel.setId(1L);
+        hotel.setCity("Warsaw");
+        hotel.setCountry("Poland");
+        hotel.setIsAvailableOnSummer(true);
+        hotel.setStars(4);
+
+        hotelRoom = new HotelRoom();
+        hotelRoom.setId(1L);
+        hotelRoom.setHasPrivateToilet(true);
+        hotelRoom.setIsAvailable(true);
+        hotelRoom.setNumberOfBeds(2);
+        hotel.addHotelRoom(hotelRoom);
+
+        client = new Client();
+        client.setId(1L);
+        client.setEmail("test@gmail.com");
+        client.setCity("Paris");
+        client.setCountry("France");
+        client.setDateOfBirth(LocalDate.of(2000, 3, 15));
+        client.setPostCode("23-444");
+        client.setPassword("Password12!");
+        client.setHouseNumber(44);
+
+        booking = new Booking();
+        booking.setClient(client);
+        booking.setId(1L);
+        booking.setHotelRoom(hotelRoom);
+        booking.setAccommodationDate(LocalDate.now());
+        booking.setEvictionDate(LocalDate.of(2022, 7, 30));
+
     }
 
     @Test
@@ -51,7 +98,7 @@ class ClientServiceTest {
         // when
         underTest.findAllClients();
         // then
-        verify(clientRepository).deleteAll();
+        verify(clientRepository).findAll();
     }
 
     @Test
@@ -79,16 +126,43 @@ class ClientServiceTest {
         assertEquals(clientRequest.getStreet(), clientDto.getStreet());
         assertEquals(clientRequest.getPostCode(), clientDto.getPostCode());
         assertEquals(clientRequest.getHouseNumber(), clientDto.getHouseNumber());
-
-    }
-
-    @Test
-    @Disabled
-    void findClient() {
     }
 
     @Test
     @Disabled
     void deleteClient() {
+    }
+
+    @Test
+    @Transactional
+    void canAddClientToHotelRoom() {
+        // given
+        BookingRequest bookingRequest = new BookingRequest(booking.getClient().getId(), booking.getHotelRoom().getId(),
+                booking.getAccommodationDate(), booking.getEvictionDate()
+                );
+        // when
+        clientRepository.save(client);
+        hotelRepository.save(hotel);
+        hotelRoomRepository.save(hotelRoom);
+        BookingDto bookingDto = underTest.addClientToHotelRoom(bookingRequest);
+
+        // then
+//        Client client = clientRepository.findByEmail(bookingDto.getClient().getEmail()).get();
+//        assertEquals(bookingRequest.getClientId(), client.getId());
+    }
+
+    @Test
+    void findClientByEmail() {
+        // given
+        String email = "test@gmail.com";
+        Mockito.when(clientRepository.findByEmail(email)).thenReturn(Optional.of(client));
+
+        // when
+        ClientDto clientTest = underTest.findClientByEmail(email);
+
+        // then
+        Assertions.assertNotNull(client);
+        assertEquals(client.getEmail(), clientTest.getEmail());
+
     }
 }
