b5f62065082d42ce02fc37141052d0d84d3b529a
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-06-12 23:29:55 +0200

    implemented finding client by email

--- src/main/java/com/maxamato/bookingsystem/repository/ClientRepository.java
+++ src/main/java/com/maxamato/bookingsystem/repository/ClientRepository.java
@@ -2,11 +2,16 @@ package com.maxamato.bookingsystem.repository;
 
 import com.maxamato.bookingsystem.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long>{
 
     boolean existsByEmail(String email);
-    Client findByEmail(String email);
+
+    @Query(value = "select * from Client c where c.email = ?1")
+    Optional<Client> findByEmail(String email);
 }
