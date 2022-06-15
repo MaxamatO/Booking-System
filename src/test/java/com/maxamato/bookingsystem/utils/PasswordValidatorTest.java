9c0b1aeda5cac3f6dafd45cd29114e1fec019e18
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-06-15 23:49:03 +0200

    Tested PasswordValidator.java

--- src/test/java/com/maxamato/bookingsystem/utils/PasswordValidatorTest.java
+++ src/test/java/com/maxamato/bookingsystem/utils/PasswordValidatorTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordValidatorTest {
 
@@ -18,15 +21,25 @@ public class PasswordValidatorTest {
 
     @ParameterizedTest
     @CsvSource({
-            "1234abc, false",
-            "1234abcd1234abcd@#$@$$fsdf#$$$$$, false",
-            "withoutupeprcase, false",
-            "WithoutSpecialChar, false",
-            "WithoutANumber, false",
-            "Password123!, true"
+            "1234abc, Password must contain 8-20 characters.",
+            "1234abcd1234abcd@#$@$$fsdf#$$$$$, Password must contain 8-20 characters.",
+            "withoutupeprcase, Password must contain at least one upper case character.",
+            "WithoutSpecial5, Password must contain at least one special character.",
+            "WithoutANumber!, Password must contain at least one number",
+            "WHEREISLOWERCASE, Password can't contain only upper case characters."
     })
-    void itShouldValidatePassword(String password, boolean expected){
-        boolean isValid = underTest.test(password);
-        assertThat(isValid).isEqualTo(expected);
+    void itShouldValidatePassword_DoesNotPass(String password, String expectedException){
+        Exception exception = assertThrows(IllegalStateException.class, () ->
+                underTest.test(password));
+        String actualException = exception.getMessage();
+        assertTrue(actualException.contains(expectedException));
     }
+
+    @Test
+    void itShouldValidatePassword_Passes(){
+        boolean isValid = underTest.test("Password123!");
+        assertThat(isValid).isEqualTo(true);
+    }
+
+
 }
