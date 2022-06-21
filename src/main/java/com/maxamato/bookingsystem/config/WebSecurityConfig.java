a0e94380e57190d4c68e0853d6764b332c965f92
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-06-21 23:10:09 +0200

    Implementation of basic auth to be continued

--- src/main/java/com/maxamato/bookingsystem/config/WebSecurityConfig.java
+++ src/main/java/com/maxamato/bookingsystem/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.maxamato.bookingsystem.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -8,25 +9,53 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.maxamato.bookingsystem.config.ApplicationUserRole.ADMIN;
+import static com.maxamato.bookingsystem.config.ApplicationUserRole.CLIENT;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/v*/booking_system/**")
+                .antMatchers("/", "index", "/css/*", "/js/*")
                 .permitAll()
-                .anyRequest().authenticated();
-
-
-
+                .antMatchers("/api/v1/booking_system/**").hasRole(CLIENT.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
-
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails maksUser = User.builder()
+                .username("maks")
+                .password(passwordEncoder.encode("password"))
+                .roles(CLIENT.name())
+                .build();
+        UserDetails kubaUser = User.builder()
+                .username("kuba")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMIN.name())
+                .build();
+        return new InMemoryUserDetailsManager(
+                maksUser,
+                kubaUser
+        );
+    }
 }
